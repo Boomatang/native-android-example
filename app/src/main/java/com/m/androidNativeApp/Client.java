f0a35bce72ec9c860d7c3bc0175e85a9b32b3325
Michal Stokluska <mstoklus@redhat.com>
2019-11-05 09:38:24 +0000

    feat:subscriptions

--- app/src/main/java/com/m/androidNativeApp/Client.java
+++ app/src/main/java/com/m/androidNativeApp/Client.java
@@ -1,6 +1,17 @@
 package com.m.androidNativeApp;
 
 import com.apollographql.apollo.ApolloClient;
+import com.apollographql.apollo.api.Operation;
+import com.apollographql.apollo.api.ResponseField;
+import com.apollographql.apollo.cache.normalized.CacheKey;
+import com.apollographql.apollo.cache.normalized.CacheKeyResolver;
+import com.apollographql.apollo.cache.normalized.NormalizedCacheFactory;
+import com.apollographql.apollo.cache.normalized.lru.EvictionPolicy;
+import com.apollographql.apollo.cache.normalized.lru.LruNormalizedCacheFactory;
+import com.apollographql.apollo.subscription.WebSocketSubscriptionTransport;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import okhttp3.OkHttpClient;
 
@@ -8,14 +19,43 @@ public class Client {
 
     private static final String SERVER_URL = "https://ionic-showcase-server-customer-a-shar-b4c5.apps.ire-85ac.open.redhat.com/graphql";
 
-    public static ApolloClient setupApollo() {
+    public static final ApolloClient apolloClient = setupApollo();
+
+    public static NormalizedCacheFactory normalizedCacheFactory(){
+        return new LruNormalizedCacheFactory(EvictionPolicy.builder().maxSizeBytes(10 * 1024).build());
+    }
+
+    public static CacheKeyResolver cacheKeyResolver(){
+        return new CacheKeyResolver() {
+            @Override
+            public CacheKey fromFieldRecordSet(ResponseField field, Map<String, Object> recordSet) {
+                if (recordSet.containsKey("id")) {
+                    String id = (String) recordSet.get("id");
+                    return CacheKey.from(id);
+                }
+                return CacheKey.NO_KEY;
+            }
+
+            @Override
+            public CacheKey fromFieldArguments(ResponseField field, Operation.Variables variables) {
+                return CacheKey.NO_KEY;
+            }
+        };
+    }
+
+    private static ApolloClient setupApollo() {
 
         OkHttpClient okHttpClient = new OkHttpClient
                 .Builder()
                 .build();
 
+        Map<String, Object> connectionParams = new HashMap<>();
+
         return ApolloClient.builder()
                 .serverUrl(SERVER_URL)
+                .normalizedCache(normalizedCacheFactory(), cacheKeyResolver())
+                .subscriptionConnectionParams(connectionParams)
+                .subscriptionTransportFactory(new WebSocketSubscriptionTransport.Factory(SERVER_URL, okHttpClient))
                 .okHttpClient(okHttpClient)
                 .build();
     }
--- app/src/main/java/com/m/androidNativeApp/MainActivity.java
+++ app/src/main/java/com/m/androidNativeApp/MainActivity.java
@@ -10,9 +10,10 @@ import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 import android.os.Bundle;
+import android.widget.EditText;
 
 import com.apollographql.apollo.ApolloCall;
-import com.apollographql.apollo.ApolloClient;
+import com.apollographql.apollo.ApolloSubscriptionCall;
 import com.apollographql.apollo.api.Response;
 import com.apollographql.apollo.exception.ApolloException;
 
@@ -20,10 +21,10 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 
+import static com.m.androidNativeApp.Client.apolloClient;
+
 public class MainActivity extends AppCompatActivity {
 
-    // private static final ApolloClient client = new Client().setupApollo();
-    //creating instance of Item Adapter Class with recycler view
     private String taskTitle, taskDescription, taskId;
     RecyclerView recyclerView;
     ItemAdapter itemAdapter;
@@ -35,48 +36,115 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // initialize item list
         itemList = new ArrayList<>();
-
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
-
-        // setting recycler view to be vertical ( list of items 1 by 1 going top to bottom)
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        AllTasksQuery tasksQuery = AllTasksQuery
+        itemAdapter = new ItemAdapter(this, itemList);
+
+
+        MainActivity.this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                recyclerView.setAdapter(itemAdapter);
+            }
+        });
+
+        getTasks();
+        subscribeToAddTask();
+        subscribeToDeleteTask();
+    }
+
+    public void subscribeToDeleteTask() {
+
+        DeleteTaskSubscription deleteTaskSubscription = DeleteTaskSubscription
                 .builder()
                 .build();
 
-        Client.setupApollo().query(tasksQuery).enqueue(new ApolloCall.Callback<AllTasksQuery.Data>() {
+        apolloClient.clearNormalizedCache();
+        apolloClient.subscribe(deleteTaskSubscription).execute(new ApolloSubscriptionCall.Callback<DeleteTaskSubscription.Data>() {
             @Override
-            public void onResponse(@NotNull Response<AllTasksQuery.Data> response) {
+            public void onResponse(@NotNull Response<DeleteTaskSubscription.Data> response) {
 
-                final AllTasksQuery.Data mResponse = response.data();
-
-                final int dataLength = mResponse.allTasks().size();
-
-                for (int i = 0; i < dataLength; i++) {
-                    taskTitle = mResponse.allTasks().get(i).fragments().taskFields().title();
-                    taskDescription = mResponse.allTasks().get(i).fragments().taskFields().description();
-                    taskId = mResponse.allTasks().get(i).fragments().taskFields().id();
-                    itemList.add(new Item(taskTitle, taskDescription, taskId));
+                for (Item item : itemList) {
+                    if (item.id.equals(response.data().taskDeleted.fragments().taskFields.id())) {
+                        itemToRemove = item;
+                    }
                 }
 
-                itemAdapter = new ItemAdapter(MainActivity.this, itemList);
+                itemList.remove(itemToRemove);
 
-                MainActivity.this.runOnUiThread(new Runnable() {
+                runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        recyclerView.setAdapter(itemAdapter);
+                        itemAdapter.notifyDataSetChanged();
+                    }
+                });
+
+            }
+
+            @Override
+            public void onFailure(@NotNull ApolloException e) {
+                System.out.println("Failed" + e);
+            }
+
+            @Override
+            public void onCompleted() {
+                System.out.println("Subscribed to DeleteTask");
+            }
+
+            @Override
+            public void onTerminated() {
+                System.out.println("DeleteTask subscription terminated");
+            }
+
+            @Override
+            public void onConnected() {
+                System.out.println("Connected to DeleteTask subscription");
+            }
+        });
+    }
+
+    public void subscribeToAddTask() {
+        AddTaskSubscription addTaskSubscription = AddTaskSubscription
+                .builder()
+                .build();
+        apolloClient.clearNormalizedCache();
+        apolloClient.subscribe(addTaskSubscription).execute(new ApolloSubscriptionCall.Callback<AddTaskSubscription.Data>() {
+            @Override
+            public void onResponse(@NotNull Response<AddTaskSubscription.Data> response) {
+                itemList.add(new Item(response.data().taskAdded.fragments().taskFields.title(), response.data().taskAdded.fragments().taskFields.description()
+                        , response.data().taskAdded.fragments().taskFields.id()));
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        itemAdapter.notifyDataSetChanged();
                     }
                 });
             }
 
             @Override
             public void onFailure(@NotNull ApolloException e) {
-                System.out.println(e);
+                System.out.println("Failed " + e);
+            }
+
+            @Override
+            public void onCompleted() {
+                System.out.println("Subscribed to AddTask");
+            }
+
+            @Override
+            public void onTerminated() {
+                System.out.println("AddTask subscription terminated");
+            }
+
+            @Override
+            public void onConnected() {
+                System.out.println("Connected to AddTask subscription");
             }
         });
+
     }
 
     public void deleteTask(View view) {
@@ -90,12 +158,12 @@ public class MainActivity extends AppCompatActivity {
                 .id(buttonId)
                 .build();
 
-        Client.setupApollo().mutate(deleteTask).enqueue(new ApolloCall.Callback<DeleteTaskMutation.Data>() {
+        apolloClient.mutate(deleteTask).enqueue(new ApolloCall.Callback<DeleteTaskMutation.Data>() {
             @Override
             public void onResponse(@NotNull final Response<DeleteTaskMutation.Data> response) {
 
                 for (Item item : itemList) {
-                    if (item.id.equals(response.data().deleteTask())) {
+                    if (response.data() != null && item.id.equals(response.data().deleteTask())) {
                         itemToRemove = item;
                     }
                 }
@@ -121,6 +189,41 @@ public class MainActivity extends AppCompatActivity {
     public void addTask(View view) {
         Intent launchActivity1 = new Intent(this, CreateTask.class);
         startActivity(launchActivity1);
+    }
+
+    public void getTasks() {
+
+        AllTasksQuery tasksQuery = AllTasksQuery
+                .builder()
+                .build();
+
+        apolloClient.query(tasksQuery).enqueue(new ApolloCall.Callback<AllTasksQuery.Data>() {
+            @Override
+            public void onResponse(@NotNull Response<AllTasksQuery.Data> response) {
+                final AllTasksQuery.Data mResponse = response.data();
+
+                final int dataLength = mResponse.allTasks().size();
+
+                for (int i = 0; i < dataLength; i++) {
+                    taskTitle = mResponse.allTasks().get(i).fragments().taskFields().title();
+                    taskDescription = mResponse.allTasks().get(i).fragments().taskFields().description();
+                    taskId = mResponse.allTasks().get(i).fragments().taskFields().id();
+                    itemList.add(new Item(taskTitle, taskDescription, taskId));
+                }
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        itemAdapter.notifyDataSetChanged();
+                    }
+                });
+            }
+
+            @Override
+            public void onFailure(@NotNull ApolloException e) {
+                System.out.println(e);
+            }
+        });
     }
 
 }
